f35f7c15a4169808f4dbe6792016de33850ae3bc
Roberto Paz <fsjedi@outlook.com>
2021-09-19 12:50:43 -0400

    corregimos dias

--- trainingDays.js
+++ trainingDays.js
@@ -15,11 +15,11 @@ const getRandEvent = () => {
 const getTrainingDays = (event) => {
   let days
   if (event === 'Marathon') {
-    let days = 50
+    days = 50
   } else if (event === 'Triathlon') {
-    let days = 100
+    days = 100
   } else if (event === 'Pentathlon') {
-    let days = 200
+    days = 200
   }
 
   return days
