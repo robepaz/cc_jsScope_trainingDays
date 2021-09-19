263c71f49f76a39e6b790b82d0aaef2276e9a231
Roberto Paz <fsjedi@outlook.com>
2021-09-19 13:03:36 -0400

    final

--- trainingDays.js
+++ trainingDays.js
@@ -1,53 +1,51 @@
-// The scope of `random` is too loose 
-const random = Math.floor(Math.random() * 3);
-const name = 'Nala';
+// The scope of `random` is too loose
+
+const name = 'Nala'
 
 const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3)
   if (random === 0) {
-    return 'Marathon';
+    return 'Marathon'
   } else if (random === 1) {
-    return 'Triathlon';
+    return 'Triathlon'
   } else if (random === 2) {
-    return 'Pentathlon';
+    return 'Pentathlon'
   }
-};
+}
 
-// The scope of `days` is too tight 
-const getTrainingDays = event => {
-  let days;
+// The scope of `days` is too tight
+const getTrainingDays = (event) => {
+  let days
   if (event === 'Marathon') {
-    days = 50;
+    days = 50
   } else if (event === 'Triathlon') {
-    days = 100;
+    days = 100
   } else if (event === 'Pentathlon') {
-    days = 200;
+    days = 200
   }
 
-  return days;
-};
+  return days
+}
 
-// The scope of `name` is too tight 
+// The scope of `name` is too tight
 const logEvent = (name, event) => {
-  
-  console.log(`${name}'s event is: ${event}`);
-};
+  console.log(`${name}'s event is: ${event}`)
+}
 
 const logTime = (name, days) => {
-  
-  console.log(`${name}'s time to train is: ${days} days`);
-};
+  console.log(`${name}'s time to train is: ${days} days`)
+}
 
-const event = getRandEvent();
-const days = getTrainingDays(event);
-// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+const event = getRandEvent()
+const days = getTrainingDays(event)
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime
 
+logEvent(name, event)
+logTime(name, days)
 
-logEvent(event);
-logTime(days);
+const event2 = getRandEvent()
+const days2 = getTrainingDays(event2)
+const name2 = 'Warren'
 
-const event2 = getRandEvent();
-const days2 = getTrainingDays(event2);
-const name2 = 'Warren';
- 
-logEvent(name2, event2);
-logTime(name2, days2);
+logEvent(name2, event2)
+logTime(name2, days2)
